fd7665ae0b9b34e437ac383afe2e2159fb66ba8a
Corey Beres <corey.beres@gmail.com>
2017-09-13 20:55:55 -0400

    add assemble task

--- backend/src/main/java/net/seabears/campsites/filters/SimpleCorsFilter.java
+++ backend/src/main/java/net/seabears/campsites/filters/SimpleCorsFilter.java
@@ -2,12 +2,17 @@ package net.seabears.campsites.filters;
 
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class SimpleCORSFilter implements Filter {
+public class SimpleCorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
