25ae57007cc0da9a6355ab9c7beb84d4acb5aa83
Corey Beres <corey.beres@gmail.com>
2018-04-26 00:55:41 -0400

    fix 404 test

--- frontend/test/unit/specs/404.spec.js
+++ frontend/test/unit/specs/404.spec.js
@@ -2,6 +2,12 @@ import View404 from '@/router/views/404'
 
 describe('@views/404', () => {
   it('is a valid view', () => {
-    expect(View404).toBeAViewComponent()
+    console.log(View404)
+    expect(View404).to.exist
+    expect(View404.components).to.exist
+    expect(View404.components.Layout).to.exist
+    expect(View404.page).to.exist
+    expect(View404.page.title).to.exist
+    expect(View404.page.meta).to.exist
   })
 })
