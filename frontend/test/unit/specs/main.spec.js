5b73a00044a65074533cfc4f37e0532cdc6ed6e8
Corey Beres <corey.beres@gmail.com>
2018-04-30 21:33:51 -0400

    fix main layout test

--- frontend/test/unit/specs/main.spec.js
+++ frontend/test/unit/specs/main.spec.js
@@ -1,18 +1,16 @@
 import Vue from 'vue'
-import vueTestUtils from '@vue/test-utils'
 import MainLayout from '@/router/layouts/main'
 
 describe('@layouts/main', () => {
   it('renders its content', () => {
     const slotContent = '<p>Hello!</p>'
-    const comp = new Vue({
-      template: slotContent
+    const slotComp = new Vue({
+      components: { MainLayout },
+      template: '<MainLayout>' + slotContent + '</MainLayout>'
     })
-    const { element } = vueTestUtils.shallow(MainLayout, {
-      slots: {
-        default: comp
-      }
-    })
-    expect(element.innerHTML).to.include(slotContent)
+    const vm = slotComp.$mount()
+    const tags = vm.$el.getElementsByTagName('el-main')
+    expect(tags.length).to.equal(1)
+    expect(tags[0].innerHTML).to.equal(slotContent)
   })
 })
